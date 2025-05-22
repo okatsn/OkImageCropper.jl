45d6e75ca9d8d42605485289a33111e81e5c9ad2
Documenter.jl <documenter@juliadocs.github.io>
2025-05-22 11:24:34 +0000

    build based on 21f5e76

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OkImageCropper","page":"Home","title":"OkImageCropper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkImageCropper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
